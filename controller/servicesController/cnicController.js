053f430e57fdd33bf2da006d7bccfcc2a16b380e
husnain3964 <husnain3964@gmail.com>
2026-01-02 19:06:45 +0500

    uploaded
    create cnic client

--- controller/servicesController/cnicController.js
+++ controller/servicesController/cnicController.js
@@ -42,7 +42,17 @@ const createCnic = async (req, res) => {
       fatherCnicPicFront: frontUrl,
     });
 
-   
+    await serviceModel.create({
+      clientId:cnic.clientID,
+      serviceType:"PASSPORT",
+      serviceRefId:cnic._id
+    })
+    return res.status(200).json({
+      success: true,
+      message: "data recived for new cnic",
+      data: cnic,
+      id: cnic.id,
+    });
   } catch (error) {
     return res.status(500).json({
       success: false,
