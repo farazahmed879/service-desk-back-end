6d4f846f125b20f5e5bd76a73019b0c08ea2a7c9
husnain3964 <husnain3964@gmail.com>
2026-03-02 17:56:02 +0500

    electric-meterCrud-complete

--- controller/electric-utilty-contrller/connectionController.js
+++ controller/electric-utilty-contrller/connectionController.js
@@ -1,5 +1,6 @@
 const EconnectionModel = require("../../models/electricUtiltyModels/connection")
  const cloudinary = require("../../config/cloudinary")
+const { updateBformClientfromId } = require("../servicesController/bFormController")
     
 const createConnection = async  (req,res)=>{
 
@@ -85,7 +86,127 @@ return res.status(200).json({
 }
 
 
+const getAllConnection = async (req , res)=>{
+
+    try{
+    const allConnection = await EconnectionModel.find().populate("clientID")
+
+    if(allConnection){
+        return res.status(200).json({
+            data:allConnection,
+            message:"success"
+        })
+    }
+    }
+    catch(error){
+        return res.status(400).json({
+            success:false,
+            message:"something went wrong",
+            data:error.message
+        })
+    }
+}
+
+const updateConnection = async (req,res)=>{
+    try {
+        const {id , connectionType , meterNumber, status , clientID} = req.body
+    if(!id){
+        return res.status(400).json({
+            message:"id must be required"
+        })
+    }
+
+   const getdata = await EconnectionModel.findById(id)
+   if(!getdata){
+    return res.status(400).json({
+        message:"record not found with this id"
+    })
+
+   } 
+ let updateData ={}
+ if (connectionType){
+   updateData.connectionType = connectionType   
+ }
+ 
+ if (meterNumber){
+   updateData.meterNumber = meterNumber   
+ }
+ 
+ if (status){
+   updateData.status= status   
+ }
+ 
+ if (clientID){
+   updateData.clientID = clientID   
+ }
+ 
+
+ 
+  const update1 = req.files?.cnicFrontImg?.[0];   
+  const update2 = req.files?.cnicBackImg?.[0];
+  if(update1){
+    const  upload1 = await cloudinary.uploader.upload(update1.path,{
+        folder:"meterConnectionDoc"
+    })
+    updateData.cnicFrontImg=upload1.secure_url
+  }   
+  if(update2){
+    const  upload2 = await cloudinary.uploader.upload(update2.path,{
+        folder:"meterConnectionDoc"
+    })
+    updateData.cnicBackImg=upload2.secure_url
+  }   
+  
+  const updateMeterData = await EconnectionModel.findByIdAndUpdate(id , updateData,{
+    new:true,
+    runValidators:true
+  })
+
+  return res.status(200).json(
+    {message:"successFully update record",
+    data:updateMeterData}
+  )
+   
+
+    } catch (error) {
+         return res.status(400).json({
+            success:false,
+            message:"something went wrong",
+            data:error.message
+        })
+    }
+
+}
+
+
+ const deleterecord = async (req,res)=>{
+ try {
+       const {id}=req.params
+   if(!id){
+    return res.status(400).json({
+        message:"id must be required",
+        success:false
+    })
+   }
+ const deletee = await EconnectionModel.findByIdAndDelete(id)
+ return res.status(200).json({
+    message:"record delete",
+    data:deletee
+ }) 
+
+
+ } catch (error) {
+     return res.status(400).json({
+        message:error.message,
+        success:false
+    })
+    
+ }
+ }
 
 module.exports={
     createConnection,
+    getAllConnection,
+    updateConnection,
+    deleterecord,
 }
